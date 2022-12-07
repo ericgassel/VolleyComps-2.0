c51bf210346b6a1b5c8331e508ddef52708ea2f3
shaqok <shaqok8@gmail.com>
2022-12-07 12:51:44 +0900

    Formatted TabNav

--- frontend/src/components/TabNav.tsx
+++ frontend/src/components/TabNav.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import {Link, useLocation} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
-const  TabNav=() =>{
-
+const TabNav = () => {
   const location = useLocation()
 
   return <div className='tab-nav'>
-          <Link to="/teams/playerStatsTab" className={location.pathname==='/teams/playerStatsTab'?'tab_active':''}>Player Stats</Link>
-         <Link to="/teams/teamStatsTab" className={location.pathname==='/teams/teamStatsTab'?'tab_active':''}>Team Stats</Link>
-         <Link to="/teams/rotationsTab " className={location.pathname==='/teams/rotationsTab'?'tab_active':''}>Rotations</Link>
-         <Link to="/teams/playsTab " className={location.pathname==='/teams/playsTab'?'tab_active':''}>Plays</Link>
+    <Link to="/teams/playerStatsTab" className={location.pathname==='/teams/playerStatsTab'?'tab_active':''}>Player Stats</Link>
+    <Link to="/teams/teamStatsTab" className={location.pathname==='/teams/teamStatsTab'?'tab_active':''}>Team Stats</Link>
+    <Link to="/teams/rotationsTab" className={location.pathname==='/teams/rotationsTab'?'tab_active':''}>Rotations</Link>
+    <Link to="/teams/playsTab" className={location.pathname==='/teams/playsTab'?'tab_active':''}>Plays</Link>
   </div>;
 }
 
