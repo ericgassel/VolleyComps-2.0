ee8d3a3a1999d04809882f786c1d4f94e551f5be
shaqok <shaqok8@gmail.com>
2023-01-05 13:06:21 -0600

    Added Player number and name columns

--- frontend/src/components/GeneralTeamStats.tsx
+++ frontend/src/components/GeneralTeamStats.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+/**
+ * Option 1: https://react-pdf-viewer.dev/
+ * Option 2: https://github.com/wojtekmaj/react-pdf
+ * 
+ * I checked Bethel Univ's stats page and it seems like they are getting their stats from the same source.
+ * If we can get all the schools' stats pdf from the same source, we could embed the pdf viewer instead.
+ */
+
 // Temporary mock data
-const tableTitles = ['K', 'E', 'TA', 'PCT', 'AST', 'SA', 'SE', 'RE', 'DIG', 'BS', 'BA', 'BE', 'TB', 'BHE'];
-const mockData = [36, 12, 89, 0.270,	35,	17,	6, 2,	37,	1, 12, 4,	7, 0];
+const tableTitles = ['#', 'Player', 'K', 'E', 'TA', 'PCT', 'AST', 'SA', 'SE', 'RE', 'DIG', 'BS', 'BA', 'BE', 'TB', 'BHE'];
+const mockData = [5, 'Dani Friedges', 36, 12, 89, 0.270,	35,	17,	6, 2,	37,	1, 12, 4,	7, 0];
 
 const GeneralTeamStats = () => {
   return (
