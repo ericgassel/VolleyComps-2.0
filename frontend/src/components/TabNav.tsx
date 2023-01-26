d2597ff63e559b2708ad12520f5b909bf4ea50a0
shaqok <shaqok8@gmail.com>
2023-01-26 02:46:19 -0600

    Add teamID for routing

--- frontend/src/components/TabNav.tsx
+++ frontend/src/components/TabNav.tsx
@@ -3,12 +3,13 @@ import { Link, useLocation } from 'react-router-dom'
 
 const TabNav = () => {
   const location = useLocation()
-
+  const teamID = 0;
+  // console.log('location:', location)
   return <div className='tab-nav'>
-    <Link to="/report/St.Olaf/playerStatsTab" className={location.pathname==='/report/St.Olaf/playerStatsTab'?'tab_active':''}>Player Stats</Link>
-    <Link to="/report/St.Olaf/teamStatsTab" className={location.pathname==='/report/St.Olaf/teamStatsTab'?'tab_active':''}>Team Stats</Link>
-    <Link to="/report/St.Olaf/rotationsTab" className={location.pathname==='/report/St.Olaf/rotationsTab'?'tab_active':''}>Rotations</Link>
-    <Link to="/report/St.Olaf/playsTab" className={location.pathname==='/report/St.Olaf/playsTab'?'tab_active':''}>Plays</Link>
+    <Link to={`/report/${teamID}/playerStatsTab`} className={location.pathname===`/report/${teamID}/playerStatsTab` ?'tab_active':''}>Player Stats</Link>
+    <Link to={`/report/${teamID}/teamStatsTab`} className={location.pathname===`/report/${teamID}/teamStatsTab` ?'tab_active':''}>Team Stats</Link>
+    <Link to={`/report/${teamID}/rotationsTab`} className={location.pathname===`/report/${teamID}/rotationsTab` ?'tab_active':''}>Rotations</Link>
+    <Link to={`/report/${teamID}/playsTab`} className={location.pathname===`/report/${teamID}/playsTab` ?'tab_active':''}>Plays</Link>
   </div>;
 }
 
