5a27483f5c4ec42eda7db24918f3c0fc8b158941
shaqok <shaqok8@gmail.com>
2023-02-09 14:47:15 -0600

    Implementing Rotations tab

--- frontend/src/components/Rotation.tsx
+++ frontend/src/components/Rotation.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import * as d3 from 'd3';
 
-const Rotation = () => {
+
+const RotationChart = ({ line }: { line: any }) => {
+  const x_scale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([100, 600]);
+
+  const y_scale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([0, 600]);
+
+  console.log('line:', line);
+
+  return (
+    <svg className='rotationChartSVG' width={500} height={500}>
+      <g>
+        {line && line.map((data: any, i: React.Key) => 
+          <rect key={i} x={data.x - 200} y={data.y - 50} stroke={data.color} strokeWidth={2} width={4} height={4}></rect>
+        )}
+      </g>
+    </svg>
+  )
+}
+
+const Rotation = ({ rotation }: {rotation: any}) => {
   const lineUpArr = [1, 5, 2, 15, 3, 6];
+  const { rotation_number, line,	blocking_scheme, serve_recieve } = rotation;
+
   return (
     <div className='RotationContainer'>
       <div className='lineUpContainer'>
@@ -10,7 +36,7 @@ const Rotation = () => {
 
       <div className='notesContainer'>Notes</div>
 
-      <div className='rotationSVGContainer'></div>
+      <div className='rotationSVGContainer'><RotationChart line={line} /></div>
     </div>
   )
 }
--- frontend/src/components/RotationsTab.tsx
+++ frontend/src/components/RotationsTab.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getRotation } from '../action/action';
+import { useAppContext, useAppDispatchContext } from '../context/appContext';
 import Rotation from './Rotation';
 import './TabStyles.css';
 
 const  RotationsTab = () => {
+  const state = useAppContext();
+  const { api_base_url, rotations } = state;
+  const dispatch = useAppDispatchContext();
+
+  console.log('rotations:', rotations);
+
+  useEffect(() => {
+    getRotation(dispatch, `${api_base_url}/data/1D5DQnXIo3drLnXyzIxB9F4wPRgJIc1antzWAXFlCijM/rotations`)
+  }, [])
+  
+  const numOfColumns = 2;
+  const numOfRows = Math.ceil(rotations.length / numOfColumns);
+  const columnData = [];
+  for (let i = 0; i < numOfColumns; i++) {
+    columnData.push(rotations.slice(i * numOfRows, (i + 1) * numOfRows));
+  }
+
   return (
     <div className='rotationsTab'>
-      <Rotation />
-      <Rotation />
+      <div className='rotationsTab__columns'>
+        {columnData.map((column, i) => (
+          <div className='rotationsTab__column' key={i}>
+            {column.map((rotation: any, j: React.Key) => (
+              <Rotation key={j} rotation={rotation} />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
