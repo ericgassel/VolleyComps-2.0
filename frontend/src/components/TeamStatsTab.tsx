62446154de2ca8e8a6f2f83882bf479f2c507abb
shaqok <shaqok8@gmail.com>
2023-02-16 17:01:50 -0600

    Move API calls to TeamStatsTab and handle no data input

--- frontend/src/components/TeamStatsTab.tsx
+++ frontend/src/components/TeamStatsTab.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TeamsStats from './TeamsStats';
 import IndividualStats from './IndividualStats';
+import { useParams } from 'react-router-dom';
+import { useAppContext, useAppDispatchContext } from '../context/appContext';
+import { getTeamStats, getStats } from '../action/action';
 
-const  TeamStatsTab=() =>{
-  return <div className='teamStatsTab'>
-      {/* <h2>Welcome to TeamStat page</h2> */}
-      {/* <img src={require('../demo_graphic.svg').default} alt='mySvgImage' /> */}
-      <TeamsStats />
+const  TeamStatsTab=() => {
+  const state = useAppContext();
+  const { api_base_url, teams_stats, stats } = state;
+  const dispatch = useAppDispatchContext();
+  const { teamID } = useParams();
 
-      <IndividualStats />
-  </div>;
+  useEffect(() => {
+    getTeamStats(dispatch, `${api_base_url}/data/${teamID}/team_stats`);
+    getStats(dispatch, `${api_base_url}/data/${teamID}/ind_data`);
+  }, [])
+
+  console.log('teams_stats in TeamStatsTab', teams_stats)
+
+  return (
+    <div className='teamStatsTab'>
+      {(teams_stats.length && stats.length) ? (
+        <>
+          <TeamsStats teams_stats={teams_stats} />
+    
+          <IndividualStats stats={stats} />
+        </>
+      ) : (
+        <div className='noAPIContainer'>
+          <div>There is no Player Stats to show!</div>
+          <a className='shotEntryLink' href={`/management/${teamID}`}>Manage Roster</a>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default TeamStatsTab;
