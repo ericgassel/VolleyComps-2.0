a26fa89247bf21d7f9a1b902a6b8e5c154e12884
shaqok <shaqok8@gmail.com>
2023-02-23 13:22:50 -0600

    Added link to Shot entry and Rotations page

--- frontend/src/components/TabNav.tsx
+++ frontend/src/components/TabNav.tsx
@@ -5,11 +5,17 @@ const TabNav = () => {
   const location = useLocation()
   // const teamID = 0;
   const { teamID } = useParams();
-  console.log('teamID:', teamID)
+
   return <div className='tab-nav'>
-    <Link to={`/report/${teamID}/playerStatsTab`} className={location.pathname===`/report/${teamID}/playerStatsTab` ?'tab_active':''}>Player Stats</Link>
-    <Link to={`/report/${teamID}/teamStatsTab`} className={location.pathname===`/report/${teamID}/teamStatsTab` ?'tab_active':''}>Team Stats</Link>
-    <Link to={`/report/${teamID}/rotationsTab`} className={location.pathname===`/report/${teamID}/rotationsTab` ?'tab_active':''}>Rotations</Link>
+    <div>
+      <Link to={`/report/${teamID}/playerStatsTab`} className={location.pathname===`/report/${teamID}/playerStatsTab` ?'tab_active':''}>Player Stats</Link>
+      <Link to={`/report/${teamID}/teamStatsTab`} className={location.pathname===`/report/${teamID}/teamStatsTab` ?'tab_active':''}>Team Stats</Link>
+      <Link to={`/report/${teamID}/rotationsTab`} className={location.pathname===`/report/${teamID}/rotationsTab` ?'tab_active':''}>Rotations</Link>
+    </div>
+
+
+    {location.pathname === `/report/${teamID}/playerStatsTab` && <a href={`/ShotEntry/${teamID}`} className='tab-to-add-text'>Shot Entry</a>}
+    {location.pathname === `/report/${teamID}/rotationsTab` && <a href={`/Rotations/${teamID}`} className='tab-to-add-text'>Rotations</a>}
   </div>;
 }
 
